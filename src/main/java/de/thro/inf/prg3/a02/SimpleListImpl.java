03cdf124c5d1dcec44f3f4ee0b1b46939a04a88e
Verena-Eli <verena.feistle@t-online.de>
2019-10-09 11:05:00 +0200

    Test1

--- src/main/java/de/thro/inf/prg3/a02/SimpleListImpl.java
+++ src/main/java/de/thro/inf/prg3/a02/SimpleListImpl.java
@@ -5,7 +5,21 @@ package de.thro.inf.prg3.a02;
  * Created on 10/6/17.
  */
 public class SimpleListImpl implements SimpleList {
+    @Override
+    public void add(Object o) {
 
-	// TODO: Implement the required methods.
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public SimpleList filter(SimpleFilter filter) {
+        return null;
+    }
+
+    // TODO: Implement the required methods.
 
 }
